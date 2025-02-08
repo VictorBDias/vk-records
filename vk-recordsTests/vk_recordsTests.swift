f61fea5f6faa62db1bab6509d86209bd6f1ca4ff
Victor Batisttete Dias <victor.batisttete@yazo.com.br>
2025-02-08 16:20:40 -0300

    Initial Commit

---
+++ vk-recordsTests/vk_recordsTests.swift
@@ -0,0 +1,16 @@
+//
+//  vk_recordsTests.swift
+//  vk-recordsTests
+//
+//  Created by Victor Batisttete Dias on 08/02/25.
+//
+
+import Testing
+
+struct vk_recordsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
